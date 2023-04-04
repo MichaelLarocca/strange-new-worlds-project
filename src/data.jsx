347199bade9220fc793b39c8446fbba714c03722
Larocca <Larocca@RIVPRO1.net>
2023-04-04 14:21:38 -0400

    Added data page

---
+++ src/data.jsx
@@ -0,0 +1,58 @@
+const characters = [
+    {
+      img: "",  
+      name: "Christopher Pike",
+      position: "Captain",
+      rank: "Fleet Captain",
+      about: "Captain Christopher Pike is the commanding officer of the USS Enterprise during the era of the original Star Trek series. He is known for his intelligence, bravery, and strong moral character."
+    },
+    {
+      img: "",  
+      name: "Spock",
+      position: "Science Officer",
+      rank: "Commander",
+      about: "Spock is a half-human, half-Vulcan officer serving aboard the USS Enterprise. He is known for his logical and unemotional approach to problem-solving, as well as his close friendship with Captain Kirk."
+    },
+    {
+        img: "", 
+      name: "Christine Chapel",
+      position: "Head Nurse",
+      rank: "Lieutenant",
+      about: "Christine Chapel is a nurse serving aboard the USS Enterprise. She is known for her dedication to her patients and her unrequited love for Mr. Spock."
+    },
+    {
+      img: "",       
+      name: "La'an Noonien-Singh",
+      position: "Security Officer",
+      rank: "Lieutenant",
+      about: "La'an Noonien-Singh is a genetically engineered human serving aboard the USS Enterprise. She is known for her enhanced physical abilities and her struggle to come to terms with her own identity."
+    },
+    {
+      img: "", 
+      name: "Erica Ortegas",
+      position: "Navigator",
+      rank: "Lieutenant",
+      about: "Erica Ortegas is a navigator aboard the USS Enterprise in the 23rd century. She is known for her exceptional skills in astrogation and her ability to navigate the ship through complex spatial anomalies.",
+    },
+    {
+      img: "",   
+      name: "Dr. Joseph M'Benga",
+      position: "Chief Medical Officer",
+      rank: "Lieutenant Commander",
+      about: "Dr. Joseph M'Benga is the chief medical officer aboard the USS Enterprise in the 23rd century. He is known for his expertise in xenobiology and his ability to treat a wide range of injuries and illnesses.",
+    },
+    {
+      img: "",   
+      name: "Hemmer",
+      position: "Security Officer",
+      rank: "Lieutenant",
+      about: "Hemmer is a security officer aboard the USS Enterprise in the 23rd century. He is known for his dedication to his duty and his ability to handle high-pressure situations with calm and efficiency.",
+    },
+    {
+      img: "",   
+      name: "Una Chin-Riley",
+      position: "Executive Officer",
+      rank: "Commander",
+      about: "Una Chin-Riley is the executive officer aboard the USS Enterprise in the 23rd century. She is known for her exceptional leadership skills and her ability to keep the crew focused and motivated during difficult missions.",
+    },
+  ];
