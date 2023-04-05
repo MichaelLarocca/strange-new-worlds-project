a049cdf1aa6335f110ca4b3ad0b389d9a36f0fe5
Larocca <Larocca@RIVPRO1.net>
2023-04-05 16:30:55 -0400

    styled cards

--- src/App.jsx
+++ src/App.jsx
@@ -17,14 +17,14 @@ function App() {
   })
 
   return (
-    <div className="ctn-main">
+    <main className="ctn-main">
       {/* <p>Hello Strange New World</p> */}
       {/* <CardHardCoded /> */}
       {/* <Card src="../src/img/Pike.png" name="Pike" rank="Fleet Captain" position="Captin" about="Captin of the star ship Enterprise" /> */}
       {/* <Card src="../src/img/Spock.png" name="Spock" rank="Science Officer" position="Commander" about="Spock is a half-human, half-Vulcan officer serving aboard the USS Enterprise. He is known for his logical and unemotional approach to problem-solving, as well as his close friendship with Captain Kirk." /> */}
       {/* <Card src="../src/img/ChristineChapel.png" name="Christine Chapel" rank="Fleet Captain" position="Head Nurse" about="Christine Chapel is a nurse serving aboard the USS Enterprise. She is known for her dedication to her patients and her unrequited love for Mr. Spock." /> */}
       {cards}
-    </div>
+    </main>
   )
 }
 
