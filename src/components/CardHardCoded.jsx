92520a9c8456a8126727c3926436f10243476c1d
Larocca <Larocca@RIVPRO1.net>
2023-04-05 13:43:35 -0400

    Added hard coded card component

---
+++ src/components/CardHardCoded.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function CardHardCoded() {
+    return (
+        <div className="card">
+            <img src="../src/img/Pike.png" alt="Pike"/>
+            <p>Pike</p>
+            <p>Fleet Captain</p>
+            <p>Captin</p>
+            <p>Captin of the star ship Enterprise</p>
+            {/* <hr /> */}
+        </div>
+    )
+}
