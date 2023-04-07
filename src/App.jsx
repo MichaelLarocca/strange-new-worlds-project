4d3ed8bec09b285b29e7a7454ecd5aad4e3777a3
Michael J. Larocca <michaellarocca@live.com>
2023-04-07 16:17:05 -0400

    Updated hard coded card

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,38 @@
-import './App.css'
-import characters from './data.jsx'
-import Card from '../src/components/Card'
-import CardHardCoded from '../src/components/CardHardCoded'
-import Navbar from '../src/components/Nav'
-import Hero from '../src/components/Hero'
+import "./App.css";
+import characters from "./data.jsx";
+import Card from "../src/components/Card";
+import CardHardCoded from "../src/components/CardHardCoded";
+import Navbar from "../src/components/Nav";
+import Hero from "../src/components/Hero";
 
 function App() {
+	const cards = characters.map((character) => {
+		return (
+			<Card
+				key={character.key}
+				src={character.src}
+				name={character.name}
+				rank={character.rank}
+				position={character.position}
+				about={character.about}
+			/>
+		);
+	});
 
-  const cards = characters.map(character => {
-    return <Card 
-      key={character.key}
-      src={character.src}
-      name={character.name}
-      rank={character.rank}
-      position={character.position}
-      about={character.about}
-    />
-  })
-
-  return (
-    <div className="ctn">
-      <Navbar />
-      <Hero />
-      <main className="ctn-main">
-        {/* <p>Hello Strange New World</p> */}
-        {/* <CardHardCoded /> */}
-        {/* <Card src="../src/img/Pike.png" name="Pike" rank="Fleet Captain" position="Captin" about="Captin of the star ship Enterprise" /> */}
-        {/* <Card src="../src/img/Spock.png" name="Spock" rank="Science Officer" position="Commander" about="Spock is a half-human, half-Vulcan officer serving aboard the USS Enterprise. He is known for his logical and unemotional approach to problem-solving, as well as his close friendship with Captain Kirk." /> */}
-        {/* <Card src="../src/img/ChristineChapel.png" name="Christine Chapel" rank="Fleet Captain" position="Head Nurse" about="Christine Chapel is a nurse serving aboard the USS Enterprise. She is known for her dedication to her patients and her unrequited love for Mr. Spock." /> */}
-        {cards}
-      </main>
-    </div>
-  )
+	return (
+		<div className="ctn">
+			<Navbar />
+			<Hero />
+			<main className="ctn-main">
+				{/* <p>Hello Strange New World</p> */}
+				<CardHardCoded />
+				{/* <Card src="../src/img/Pike.png" name="Pike" rank="Fleet Captain" position="Captin" about="Captin of the star ship Enterprise" /> */}
+				{/* <Card src="../src/img/Spock.png" name="Spock" rank="Science Officer" position="Commander" about="Spock is a half-human, half-Vulcan officer serving aboard the USS Enterprise. He is known for his logical and unemotional approach to problem-solving, as well as his close friendship with Captain Kirk." /> */}
+				{/* <Card src="../src/img/ChristineChapel.png" name="Christine Chapel" rank="Fleet Captain" position="Head Nurse" about="Christine Chapel is a nurse serving aboard the USS Enterprise. She is known for her dedication to her patients and her unrequited love for Mr. Spock." /> */}
+				{/* {cards} */}
+			</main>
+		</div>
+	);
 }
 
-export default App
+export default App;
--- src/components/CardHardCoded.jsx
+++ src/components/CardHardCoded.jsx
@@ -1,20 +1,24 @@
 import React from "react";
-import logo from "../img/Icon-STN.png"
+import logo from "../img/Icon-STN.png";
 
 export default function CardHardCoded() {
-    return (
-        <div className="card">
-            <div className="card-header">
-                <img src={logo} />
-                <div>
-                    <p className="name">Pike</p>
-                    <p className="rank">Fleet Captain</p>
-                </div>
-            </div>
-            <img src="../src/img/Pike.png" alt="Pike"/>
-            <p>Captin</p>
-            <p>Captin of the star ship Enterprise</p>
-            {/* <hr /> */}
-        </div>
-    )
+	return (
+		<div className="card">
+			<div className="card-header">
+				<img src={logo} />
+				<div>
+					<p className="name">Pike</p>
+					<p className="rank">Fleet Captain</p>
+				</div>
+			</div>
+			<img className="img-character" src="../src/img/Pike.png" alt="Pike" />
+			<p className="position">Captin</p>
+			<p className="about">
+				Captain Christopher Pike is the commanding officer of the USS Enterprise
+				during the era of the original Star Trek series. He is known for his
+				intelligence, bravery, and strong moral character.
+			</p>
+			{/* <hr /> */}
+		</div>
+	);
 }
